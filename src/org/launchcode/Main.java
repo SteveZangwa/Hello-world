ac65d5ee988e9bc2162b6f676b1c55384cb9aa4c
SteveZangwa <61194558+SteveZangwa@users.noreply.github.com>
2020-02-22 10:07:30 +0200

    second commit

---
+++ src/org/launchcode/Main.java
@@ -0,0 +1,13 @@
+package org.launchcode;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hellowolrd");
+        int a = 2;
+        int b = 3;
+        int x = a + b;
+        System.out.println("Result: " + x);
+
+    }
+}
